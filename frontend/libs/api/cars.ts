18bfa0fcc13be42fcb4e7483c0e250e7029e95b3
Guilherme Guimarães <166334818+GuilhermePlayerDev@users.noreply.github.com>
2026-08-01 00:09:37 -0300

    All 5 entities api client

---
+++ frontend/libs/api/cars.ts
@@ -0,0 +1,38 @@
+// libs/api/cars.ts
+import { apiFetch } from "./client";
+import {Car} from "../../types/car";
+
+export async function getCars(): Promise<Car[]> {
+    return apiFetch<Car[]>("/cars");
+}
+export async function getCarById(id: number): Promise<Car> {
+    return apiFetch<Car>(`/cars/${id}`);
+}
+export async function getCarsByEngine(engineSupplier:string): Promise<Car[]> {
+     return apiFetch<Car[]>(`/cars/engine/${engineSupplier}`);
+}
+export async function getCarsBySeason(season:number): Promise<Car[]> {
+     return apiFetch<Car[]>(`/cars/season/${season}`);
+}
+export async function getCarsByTeam(team:string): Promise<Car[]> {
+     return apiFetch<Car[]>(`/cars/teams/${team}`);
+}
+
+export function createResult(data: Omit<Car, "id">): Promise<Car> {
+  return apiFetch<Car>("/cars", {
+    method: "POST",
+    body: JSON.stringify(data),
+  });
+}
+export function updateCar(id: number, data: Partial<Omit<Car, "id">>): Promise<Car> {
+  return apiFetch<Car>("/cars", {
+    method: "POST",
+    body: JSON.stringify(data),
+  });
+}
+export function deleteCar(id: number): Promise<void> {
+  return apiFetch<void>(`/cars/${id}`, {
+    method: "DELETE",
+  });
+}
+
--- frontend/libs/api/results.ts
+++ frontend/libs/api/results.ts
@@ -2,3 +2,40 @@
 import { apiFetch } from "./client";
 import {Result} from "../../types/result";
 
+export async function getResults(): Promise<Result[]> {
+    return apiFetch<Result[]>("/results");
+}
+export async function getResultById(id: number): Promise<Result> {
+    return apiFetch<Result>(`/results/${id}`);
+}
+export async function getRacesByDriver(driver: string): Promise<Result[]> {
+    return apiFetch<Result[]>(`/results/drivers/${driver}`);
+}
+export function getResultsByTeam(teamId: number): Promise<Result[]> {
+  return apiFetch<Result[]>(`/results/teams/${teamId}`);
+}
+export function getResultsBySeason(season: number): Promise<Result[]> {
+  return apiFetch<Result[]>(`/results/seasons/${season}`);
+}
+export function getResultsByRace(raceId: number): Promise<Result[]> {
+  return apiFetch<Result[]>(`/results/races/${raceId}`);
+}
+
+export function createResult(data: Omit<Result, "id">): Promise<Result> {
+  return apiFetch<Result>("/results", {
+    method: "POST",
+    body: JSON.stringify(data),
+  });
+}
+export function updateResult(id: number, data: Partial<Omit<Result, "id">>): Promise<Result> {
+  return apiFetch<Result>(`/results/${id}`, {
+    method: "PUT",
+    body: JSON.stringify(data),
+  });
+}
+export function deleteResult(id: number): Promise<void> {
+  return apiFetch<void>(`/results/${id}`, {
+    method: "DELETE",
+  });
+}
+
